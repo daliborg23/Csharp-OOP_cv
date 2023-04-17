f99c002218d35ee5ef4ed57d2f971dc716c41512
daliborg23 <glda@protonmail.ch>
2023-04-17 08:44:00 +0200

    1b

--- Rozhrani/Rozhrani1.cs
+++ Rozhrani/Rozhrani1.cs
@@ -7,9 +7,11 @@ using System.Threading.Tasks;
 namespace Rozhrani1 {
     abstract class Animal {
         public string name;
+        public static int countOfAnimals;
         public Animal() { }
         public Animal(string name) {
             this.name = name;
+            countOfAnimals++;
         }
     }
     class Dog : Animal {
@@ -21,7 +23,6 @@ namespace Rozhrani1 {
         public string sound() {
             return "haf";
         }
-
     }
     class Cat : Animal {
         Boolean isPedigree;
@@ -42,17 +43,32 @@ namespace Rozhrani1 {
     }
     class Rozhrani {
         public static void Mainx() {
-            Dog Pes1 = new Dog("Alik");
-            Dog Pes2 = new Dog("Azor");
-            Cat Kocka1 = new Cat ("Fous");
-            Cat Kocka2 = new Cat ("Tlapka");
-            Turtle Zelva1 = new Turtle("Tony");
-            Turtle Zelva2 = new Turtle("Greta");
-            Animal[] animals = new Animal[] {Pes1,Pes2,Kocka1,Kocka2,Zelva1,Zelva2};
-            foreach (var animal in animals) {
+            //Dog Pes1 = new Dog("Alik");
+            //Dog Pes2 = new Dog("Azor");
+            //Cat Kocka1 = new Cat ("Fous");
+            //Cat Kocka2 = new Cat ("Tlapka");
+            //Turtle Zelva1 = new Turtle("Tony");
+            //Turtle Zelva2 = new Turtle("Greta");
+            //Animal[] animals = new Animal[6];
+            //animals[0] = Pes1; animals[1] = Pes2; animals[2] = Kocka1; animals[3] = Kocka2; animals[4] = Zelva1; animals[5] = Zelva2;
+            //Animal[] animals = new Animal[] {Pes1,Pes2,Kocka1,Kocka2,Zelva1,Zelva2};
+            Animal[] animals = {
+                new Dog("Alik"),
+                new Dog("Azor"),
+                new Cat ("Fous"),
+                new Cat ("Tlapka"),
+                new Turtle("Tony"),
+                new Turtle("Greta")
+            };
+            //foreach (var animal in animals) {
+            //    //Console.WriteLine(animal.name); // 1.
+            //    Console.WriteLine($"{animal.name} - {animal.GetType().Name}"); // 2
+            //}
+            for (int i = 0;i<animals.Length;i++) {
                 //Console.WriteLine(animal.name); // 1.
-                Console.WriteLine($"{animal.name} - {animal.GetType().Name}"); // 2
+                Console.WriteLine($"{animals[i].name} - {animals[i].GetType().Name}"); // 2
             }
+            Console.WriteLine("Pocet zvirat: " + Animal.countOfAnimals);
         }
     }
 }
