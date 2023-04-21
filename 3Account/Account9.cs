24cadccbd92777ada3318de7a6da98aa2655a39b
daliborg23 <glda@protonmail.ch>
2023-04-21 12:02:12 +0200

    update

--- 3Account/Account9.cs
+++ 3Account/Account9.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -23,6 +25,7 @@ namespace Account9 {
             }
         }
         public int writeBalance() { //spis string
+            //Console.WriteLine($" je {balance},-"); // tady chci dostat nazev promenne ale asi jen pomoci property?
             return balance;
         }
         //public void writeBalance() {
@@ -61,26 +64,54 @@ namespace Account9 {
                     Console.WriteLine("Zadana zaporna castka...");
                     throw new ArgumentOutOfRangeException();
                 }
-                if (amount * (-1) < 0) {
-                    Console.WriteLine("Nedostatek penez pro prevod... Na ucte je: " + balance);
+                if (amount > this.balance) {
+                    Console.WriteLine("Nedostatek penez pro prevod... Na ucte je: " + balance + " ");
                     throw new ArgumentOutOfRangeException();
                 }
                 else {
-                    balance += amount; // +vklad -vyber
+                    this.balance -= amount;
+                    account.balance += amount;
                 }
             }
             catch (ArgumentOutOfRangeException e) {
-                Console.WriteLine(e.Message.ToString());
+                Console.WriteLine("["+e.Message.ToString()+"]");
                 //throw;
             }
         }
+        //public void transferTo(Account ucet, int castka) {
+        //    try {
+        //        if (castka > balance) {
+        //            throw new Exception("TransferTo() říká: Na účtu není dost peněz");
+        //        }
+        //        else if (castka < 0) {
+        //            throw new Exception("TransferTo() říká: Nemůžeš vysát prachy...");
+        //        }
+        //        else if (castka == 0) {
+        //            throw new Exception("TransferTo() říká: Posíláš nulu, jo? Pereš prachy, jo?");
+        //        }
+        //        else if (this == ucet) {
+        //            throw new Exception("TransferTo() říká: To se jako snažíš poslat prachy sám sobě, jo?");
+
+
+        //        }
+        //        ucet.balance += castka;
+        //        this.balance -= castka;
+
+        //    }
+        //    catch (Exception e) {
+        //        Console.WriteLine(e.Message);
+        //    }
+
+        //}
     }
     public class TestAccount {
         public static void Mainx() {
             Account u1 = new Account();
             Account u2 = new Account();
             u1.balance = 500; // startovne
-            Console.Write("Vklad/vyber penez: ");
+            Console.WriteLine("Na ucte u1: " + u1.writeBalance() + ",-");
+            Console.WriteLine("Na ucte u2: " + u2.writeBalance() + ",-");
+            Console.Write("Vklad/vyber penez na u1: ");
             int amount;
             try {
                 amount = Int32.Parse(Console.ReadLine());
@@ -95,13 +126,14 @@ namespace Account9 {
                 throw;
             }
             u1.insertInto(amount);
-            Console.WriteLine($"Zadano: {amount} \t Celkem na uctu: {u1.writeBalance()},-");
+            Console.WriteLine($"Zadano: {amount},- \t ");
+            Console.WriteLine("Na ucte u1: " + u1.writeBalance() + ",-");
+            Console.WriteLine("Na ucte u2: " + u2.writeBalance() + ",-");
             //Console.WriteLine(u1.writeBalanceInDolars(20));
             //Console.WriteLine(u1.writeBalanceInDolars(0));
             //Console.WriteLine(u1.writeBalanceInDolarsDouble(20));
             //Console.WriteLine(u1.writeBalanceInDolarsDouble(0));
-            Console.WriteLine(u1.writeBalance());
-            Console.Write("Kolik penez chcete poslat na 2. ucet?");
+            Console.WriteLine("Kolik penez chcete poslat na 2. ucet?");
             try {
                 amount = Int32.Parse(Console.ReadLine());
             }
@@ -115,6 +147,8 @@ namespace Account9 {
                 throw;
             }
             u1.transferTo(u2,amount);
+            Console.WriteLine("Na ucte u1: " + u1.writeBalance() + ",-");
+            Console.WriteLine("Na ucte u2: " + u2.writeBalance() + ",-");
         }
     }
 }
